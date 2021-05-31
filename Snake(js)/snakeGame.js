837471e425a00e67da49ef9a6008d4b8408ae375
j4ck990 <77227513+j4ck990@users.noreply.github.com>
2021-05-31 22:47:43 +0800

    Update snakeGame.js

--- Snake(js)/snakeGame.js
+++ Snake(js)/snakeGame.js
@@ -10,7 +10,7 @@ function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) {
       // window.location.replace('https://j4ck990.github.io/Snek/')
-      window.location = '/Snek/'
+      window.location = '/'
     }
     return
   }
