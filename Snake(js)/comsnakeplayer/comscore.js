fa9709db7d91b89188b2b2ca5187d0f889c79724
j4ck990 <77227513+j4ck990@users.noreply.github.com>
2021-06-29 00:18:12 +0800

    Update comscore.js

--- Snake(js)/comsnakeplayer/comscore.js
+++ Snake(js)/comsnakeplayer/comscore.js
@@ -10,7 +10,7 @@ export function update() {
     const max_steps = GRID_SIZE * GRID_SIZE - snakeBody.length
     score += base_points + Math.floor(bonus_points*(max_steps - steps)/max_steps)
     steps = 0
-    console.log("Player 2: " + score)
+    console.log("AI: " + score)
 }
 
 export function draw(gameboard) {
